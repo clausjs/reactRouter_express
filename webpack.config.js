a6953f4b11502d7ccfa4131345dfd2b8dc471986
Joseph Claus <joseph.s.claus@gmail.com>
2019-10-01 12:49:25 -0400

    Update wepback config to build 'scss' or 'sass' files

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         loader: "babel-loader"
       },
       {
-        test:/\.(s*)css$/,
+        test: [/\.(s*)css$/, /\.sass$/],
         use:['style-loader','css-loader', 'sass-loader']
       }
     ]
